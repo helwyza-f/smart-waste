4156d7b782aa13ac6def025f02771e06e2011ced
helwyza-f <helwyza@gmail.com>
2025-03-24 01:24:35 +0700

    add pwa ios

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 
-function page() {
-  return <div>Hello</div>;
-}
+const HomePage = () => {
+  const [showInstallPrompt, setShowInstallPrompt] = useState(false);
 
-export default page;
+  useEffect(() => {
+    const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    const isInStandaloneMode = window.matchMedia(
+      "(display-mode: standalone)"
+    ).matches;
+
+    if (isiOS && !isInStandaloneMode) {
+      setShowInstallPrompt(true);
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Hello</h1>
+
+      {showInstallPrompt && (
+        <div className="fixed bottom-4 left-4 right-4 bg-blue-500 text-white p-3 rounded-lg shadow-lg text-center">
+          <p>
+            Install aplikasi ini: Tekan{" "}
+            <span className="font-bold">"Share"</span> lalu pilih{" "}
+            <span className="font-bold">"Add to Home Screen"</span>.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
