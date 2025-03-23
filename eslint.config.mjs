492acac4de359ff88a263e55605e89506fef36f8
helwyza-f <helwyza@gmail.com>
2025-03-24 01:07:37 +0700

    add fungsional

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-var": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
